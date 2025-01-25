30f96cef12fced482092f8292b1da03243c00231
ShannonC8 <71951666+ShannonC8@users.noreply.github.com>
2025-01-25 11:15:38 -0500

    frontend base

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Welcome to FitHer</h1>
+        <p className="App-subtitle">
+          Your personalized companion for fitness, health, and wellness!
+        </p>
+        <button className="cta-button">Get Started</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
