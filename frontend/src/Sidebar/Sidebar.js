813faeead637af42b02ea9c6b0dc1b378d8b27c7
ShannonC8 <71951666+ShannonC8@users.noreply.github.com>
2025-01-26 01:59:54 -0500

    sidebar styling

--- frontend/src/Sidebar/Sidebar.js
+++ frontend/src/Sidebar/Sidebar.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Sidebar.css'; // Add styling in a separate CSS file if needed
+import logo from './logo.png'
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       {/* Logo container */}
-      <div className="logo">
-        <img src="/logo.png " alt="Logo" />
-      </div>
+      
       <ul>
         <li><Link to="/todo">To-Do</Link></li>
-        <li><Link to="/calendar">Calendar</Link></li>
+        <li><Link to="/calender">Calendar</Link></li>
         <li><Link to="/smile">Smile</Link></li>
       </ul>
     </div>
