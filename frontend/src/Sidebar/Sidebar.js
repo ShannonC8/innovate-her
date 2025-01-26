5c3cf64286671206abd480e563d82573f91bf76b
vickyj634 <142948257+vickyj634@users.noreply.github.com>
2025-01-26 03:39:30 -0500

    Update Sidebar.js for mascot

--- frontend/src/Sidebar/Sidebar.js
+++ frontend/src/Sidebar/Sidebar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Spline from '@splinetool/react-spline/next';
 import './Sidebar.css'; // Add styling in a separate CSS file if needed
 import logo from './logo.png'
 
@@ -13,6 +14,10 @@ const Sidebar = () => {
         <li><Link to="/calender">Calendar</Link></li>
         <li><Link to="/smile">Smile</Link></li>
       </ul>
+      {/* Spline mascot */}
+      <div className="spline-container">
+      <Spline scene="https://prod.spline.design/nHB6VGUdXILVR-YO/scene.splinecode" />
+      </div>
     </div>
   );
 };
